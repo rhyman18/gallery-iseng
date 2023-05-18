358a11b46ca89db813708244cebad8dd47425cde
Ari Budiman <rhyman18@gmail.com>
2023-05-19 01:15:15 +0700

    fix(navbar): refubrish style script

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 import Main from './scripts/utils/drawerInitiator.js';
 
 Main.init({
-    button: document.querySelector('.nav__hamburger'),
-    drawer: document.querySelector('.nav__menu'),
+    button: document.querySelector('#nav__hamburger'),
+    drawer: document.querySelector('.nav__navigation'),
     content: document.querySelector('#mainContent'),
 });
