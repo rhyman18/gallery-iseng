b8e522134d7da6f8fe6687fcce56ec6a80ca98c4
Ari Budiman <rhyman18@gmail.com>
2023-05-19 11:28:36 +0700

    feat(gallery): data and images

---
+++ assets/scripts/data/galleryBeginner.js
@@ -0,0 +1,44 @@
+const DataBeginner = [
+    {
+        judul: 'Gambar Pertama',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kedua',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Ketiga',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Keempat',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kelima',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kesekian',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kesekian',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kesekian',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kesekian',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+    {
+        judul: 'Gambar Kesekian',
+        img_url: 'assets/images/gallery_beginner.png',
+    },
+];
+
+export default DataBeginner;
