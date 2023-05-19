5c18b9807cb965d52465d29178e96c1fb883ab13
Ari Budiman <rhyman18@gmail.com>
2023-05-19 11:39:47 +0700

    feat(data): integration to frontend

--- assets/scripts/views/pages/galleryBeginner.js
+++ assets/scripts/views/pages/galleryBeginner.js
@@ -1,13 +1,22 @@
+import DataBeginner from '../../data/galleryBeginner.js';
+import createListBeginner from '../templates/galleryBeginner.js';
+
 const GalleryBeginner = {
     render() {
         return `
         <div class="jumbotron">
             <h1>Welcome to the gallery</h1>
         </div>
+        <div id="gallery__beginner"></div>
         `;
     },
 
-    afterRender() {},
+    afterRender() {
+        DataBeginner.forEach((gallery) => {
+            document.querySelector('#gallery__beginner')
+                .innerHTML += createListBeginner(gallery);
+        });
+    },
 };
 
 export default GalleryBeginner;
---
+++ assets/scripts/views/templates/galleryBeginner.js
@@ -0,0 +1,6 @@
+const createListBeginner = (gallery) => `
+<div>${gallery.judul}</div>
+<img src="${gallery.img_url}" />
+`;
+
+export default createListBeginner;
