e439e3968ddba4816bd8d5c89d777e0fdabbf6de
Ari Budiman <rhyman18@gmail.com>
2023-05-21 09:57:39 +0700

    fix(advanced): jumbotron and modal backdrop

--- assets/scripts/views/pages/galleryAdvanced.js
+++ assets/scripts/views/pages/galleryAdvanced.js
@@ -2,7 +2,14 @@ const GalleryAdvanced = {
     render() {
         return `
         <div class="jumbotron">
-            <h1>Welcome to the gallery (advanced)</h1>
+            <h1>Welcome to the gallery</h1>
+        </div>
+        <div id="gallery__advanced"></div>
+        <div id="modal" class="modal__backdrop">
+            <div class="modal__close">
+                <i class="fa-solid fa-xmark fa-xl"></i>
+            </div>
+            <div id="modal__show" class="modal__content"></div>
         </div>
         `;
     },
