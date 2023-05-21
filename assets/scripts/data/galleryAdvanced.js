cc34af5b687406e0af91da920093ad956f7cb7df
Ari Budiman <rhyman18@gmail.com>
2023-05-21 10:30:27 +0700

    feat(advanced): data

---
+++ assets/scripts/data/galleryAdvanced.js
@@ -0,0 +1,70 @@
+const DataAdvanced = [
+    {
+        judul: 'Gambar Pertama',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '33.33%'
+    },
+    {
+        judul: 'Gambar Kedua',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '33.33%'
+    },
+    {
+        judul: 'Gambar Ketiga',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '33.33%'
+    },
+    {
+        judul: 'Gambar Keempat',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '100%'
+    },
+    {
+        judul: 'Gambar Kelima',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '50%'
+    },
+    {
+        judul: 'Gambar Kesekian',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '50%'
+    },
+    {
+        judul: 'Gambar Kesekian',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '20%'
+    },
+    {
+        judul: 'Gambar Kesekian',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '20%'
+    },
+    {
+        judul: 'Gambar Kesekian',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '20%'
+    },
+    {
+        judul: 'Gambar Kesekian',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '20%'
+    },
+    {
+        judul: 'Gambar Kesekian',
+        deskripsi: 'Description about your stuff will be shown right here with Text Align left when you click the image card~! And you can add more text to tell what’s your image card about so people will understand it~! And you can add your social media in this image card so people will get more interest on your stuff~! Well you can add more of text but sometimes short text are more readable too, but don’t limit your creativity ~!',
+        img_url: 'assets/images/gallery_advanced.png',
+        basis: '20%'
+    },
+];
+
+export default DataAdvanced;
